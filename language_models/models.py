71f00f75d516fe32ecb53259cd49b4c54653ff8d
Jonathan Miller <jon@jmill.dev>
2022-06-25 17:06:09 -0400

    Add AI21

--- language_models/models.py
+++ language_models/models.py
@@ -16,6 +16,52 @@ class LanguageModel(ABC):
 
 
 @dataclass
+class AI21JurassicLanguageModelSettings:
+    model_type: str = "j1-jumbo"
+    temperature: float = 1.0
+    top_p: float = 1.0
+
+
+class AI21JurassicLanguageModel(LanguageModel):
+    def __init__(
+        self,
+        api_key: str,
+        **kwargs,
+    ) -> None:
+        self.settings = AI21JurassicLanguageModelSettings(**kwargs)
+        self.api_key = api_key
+        super().__init__()
+
+    def completion_route(self, model_type) -> str:
+        return f"https://api.ai21.com/studio/v1/{model_type}/complete"
+
+    def complete(
+        self,
+        prompt: str,
+        max_tokens: int,
+        stop: list = None,
+        **kwargs: any,
+    ) -> str:
+        headers = {
+            "Authorization": f"Bearer {self.api_key}",
+            "Content-Type": "application/json",
+        }
+        payload = {
+            "prompt": prompt,
+            "maxTokens": max_tokens,
+            "temperature": kwargs.get("temperature") or self.settings.temperature,
+            "topP": kwargs.get("top_p") or self.settings.top_p,
+            "stopSequences": stop if stop else [],
+        }
+        model_type = kwargs.get("model_type") or self.settings.model_type
+        route = self.completion_route(model_type)
+        response = requests.post(route, json=payload, headers=headers)
+        completion = response.json()
+        completion_text = completion["completions"][0]["data"]["text"]
+        return completion_text
+
+
+@dataclass
 class GooseAILanguageModelSettings:
     engine_id: str = "gpt-neo-20b"
     temperature: float = 1.0
@@ -60,6 +106,5 @@ class GooseAILanguageModel(LanguageModel):
         route = self.completion_route(engine)
         response = requests.post(route, json=payload, headers=headers)
         completion = response.json()
-        print(completion)
         completion_text = completion["choices"][0]["text"]
         return completion_text
--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 
+from language_models.models import AI21JurassicLanguageModel
 from language_models.models import GooseAILanguageModel
 
 load_dotenv()
@@ -10,6 +11,15 @@ load_dotenv()
 def test_goose_ai():
     api_key = os.environ.get("GOOSEAI_API_KEY")
     client = GooseAILanguageModel(api_key=api_key)
+    prompt = "hello"
+    max_tokens = 10
+    completion = client.complete(prompt, max_tokens=max_tokens)
+    assert completion
+
+
+def test_ai21():
+    api_key = os.environ.get("AI21_API_KEY")
+    client = AI21JurassicLanguageModel(api_key=api_key)
     prompt = "hello"
     max_tokens = 10
     completion = client.complete(prompt, max_tokens=max_tokens)
